81e08c72fa88510e63b7adf2969607beffc25424
carloswm <carlos.wmarigorda@gmail.com>
2022-03-29 03:35:43 -0500

    se agregó buscador de pokemons

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,10 +3,12 @@ import React, { useState, useEffect } from 'react';
 const Card = () => {
 
   const [api, setApi] = useState([]);
+  const [name, setName] = useState('');
+  const [find, setFind] = useState('pikachu');
 
   const getApi = async () => {
     try {
-      const response = await fetch('https://pokeapi.co/api/v2/pokemon/')
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${find}`)
       const data = await response.json()
       setApi(data)
       console.log(data)
@@ -17,11 +19,34 @@ const Card = () => {
 
   useEffect(() => {
     getApi()
-  }, [])
+  }, [find])
+
+  const handleChange = (e) => {
+    setName(e.target.value.toLowerCase())
+  }
+
+  const handleSubmit = (e) => {
+    if( name !== '') setFind(name)
+    setName('')
+  }
+
+  const { front_default } = api.sprites.other.home
 
   return (
     <div>
-      Card
+      <div>
+        <input type="text"
+          onChange={ handleChange }
+          value={ name }
+        />
+        <button
+          onClick={ handleSubmit }
+        >
+          Search
+        </button>
+      </div>
+      <h1>{api.name}</h1>
+      <img src={ front_default } alt={ api.name } />
     </div>
   )
 }
