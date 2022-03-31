903ebbc51a1de5ecbe9d1872034c61c2894c8c76
carloswm <carlos.wmarigorda@gmail.com>
2022-03-31 08:44:53 -0500

    estilos card pokemon

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 const Card = () => {
 
@@ -25,28 +27,92 @@ const Card = () => {
     setName(e.target.value.toLowerCase())
   }
 
-  const handleSubmit = (e) => {
+  const handleSubmit = () => {
     if( name !== '') setFind(name)
     setName('')
   }
 
-  const { front_default } = api.sprites.other.home
+  if(api.stats === undefined) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    )
+  }
+
+  const switchColor = () => {
+    api?.stats?.find( (stat) => {
+      if( stat.stat.name === 'hp') {
+        return '#f44336'
+      } else if( stat.stat.name === 'attack') {
+        return '#4caf52'
+      } else if( stat.stat.name === 'defense') {
+        return '#2875bc'
+      } else {
+        return '#ffc107'
+      }
+    })
+  }
 
   return (
-    <div>
-      <div>
-        <input type="text"
-          onChange={ handleChange }
-          value={ name }
-        />
-        <button
-          onClick={ handleSubmit }
-        >
-          Search
-        </button>
+    <div className="card-container">
+      <div className="input-container">
+        <div className="control">
+          <input type="text"
+            onChange={ handleChange }
+            value={ name }
+            placeholder="Find pokemons by name!"
+          />
+          <button
+            onClick={ handleSubmit }
+          >
+            Search
+          </button>
+        </div>
       </div>
-      <h1>{api.name}</h1>
-      <img src={ front_default } alt={ api.name } />
+      <div className="card">
+        <div className="top-main">
+          <div className="title">
+            <h1>{api.name}</h1>
+          </div>
+          <div className="content">
+            <p className="height">{ api.height } m</p>
+            <img className="img" src={ api.sprites?.other.home.front_default } alt={ api.name } />
+            <p className="weight">{ api.weight } kg</p>
+          </div>
+          <div className="experience">
+            <p>{api.base_experience} experience</p>
+          </div>
+        </div>
+        <div className="stadistic">
+          <div className="title">
+            <p>Stadistics</p>
+          </div>
+          <div className="stats-content">
+            {
+              api?.stats?.map( (stat, index) => {
+                return (
+                  <div key={ index } className="stats">
+                    <p className="name">{ stat.stat.name }</p>
+                    <CircularProgressbar
+                      value={ stat.base_stat }
+                      text={ stat.base_stat }
+                      styles={{
+                        path: {
+                          stroke: `${console.log(switchColor())}`,
+                        },
+                        text: {
+                          fill: '#636063'
+                        }
+                      }}
+                    />
+                  </div>
+                )
+              })
+            }
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
